776afc1758718b883b4c3830b087db5f9e48900a
nishant7487 <nishantbakhatyapuri7@gmail.com>
2025-07-15 13:25:17 +0530

    15/07/25||01:24PM

---
+++ index.js
@@ -0,0 +1,18 @@
+const name="Nishant Ranjitbhai Bakhatyapuri"
+console.log(`Hello ${name}`)
+
+const j=5
+let sum=0;
+for(i=1;i<=j;i++){
+    sum+=i
+    console.log("i:",i)
+}
+console.log(sum)
+console.log("Enter Number N:")
+const n=5
+console.log(`\n3. Program Even ODD for Value ${n}`)
+if(n%2){
+    console.log("Odd Number")
+}else{
+    console.log("Even Number")
+}
